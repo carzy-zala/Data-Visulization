fa37dcd5c50cc46a56cbb9b51348a13dd2ab0052
carzy-zala <mr.zala2003@gmail.com>
2025-10-14 23:41:40 +0100

    route for all visualization page and rename View dataset  to the Dataset Matrix

--- dv_frontend/routes/index.py
+++ dv_frontend/routes/index.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
 def get_nav():
-    # ------------------------- Dashboard ---------------------------
+    # Ensure the key exists
+    if "uploaded_df" not in st.session_state:
+        st.session_state["uploaded_df"] = None
+
+    # ------------------------- Pages ---------------------------
     dashboard_page = st.Page(
         "pages/dashboard.py",
         title="Dashboard",
@@ -9,22 +13,51 @@ def get_nav():
         default=True,
     )
 
-    # ------------------------- Data ---------------------------
     upload_dataset = st.Page(
         "pages/data/upload_dataset.py",
         title="Upload Dataset",
         icon=":material/upload_file:",
     )
+
     view_dataset = st.Page(
         "pages/data/view_dataset.py",
-        title="View Dataset",
+        title="Dataset Matrix",
         icon=":material/table_view:",
     )
 
-    # ------------------------- Navigation ---------------------------
-    return ( st.navigation(
-        {
+    bar_chart = st.Page(
+        "pages/visualization/bar_chart.py",
+        title="Bar Charts",
+        icon=":material/bar_chart:",
+    )
+    
+    distribution = st.Page(
+    "pages/visualization/distribution.py",
+    title="Data Distribution",
+    icon=":material/insights:",
+    )
+    
+    line_chart = st.Page(
+    "pages/visualization/line_chart.py",
+    title="Line Charts",
+    icon=":material/insights:",
+    )
+    
+    # ------------------------- Conditional Nav ---------------------------
+    has_data = st.session_state["uploaded_df"] is not None
+
+    if has_data:
+        # Full app once data exists
+        sections = {
             "Dashboard": [dashboard_page],
             "Data": [upload_dataset, view_dataset],
+            "Visualization": [distribution,bar_chart,line_chart],
         }
-    ))
+    else:
+        # Only allow Dashboard + Upload until data is available
+        sections = {
+            "Dashboard": [dashboard_page],
+            "Data": [upload_dataset],
+        }
+
+    return st.navigation(sections)
